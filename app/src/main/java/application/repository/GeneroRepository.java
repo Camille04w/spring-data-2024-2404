ac7f9698a80d488c47854efbb4c8f044ad347356
Camille04w <vitoriacamille603@gmail.com>
2024-04-24 13:30:02 +0000

    modeloGenero

---
+++ app/src/main/java/application/repository/GeneroRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import application.model.Genero;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GeneroRepository extends CrudRepository <Genero, Long>{
+    
+}
